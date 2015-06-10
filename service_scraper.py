779013a7811481f0d54d9573efcb294233f868a7
Forest Gregg <fgregg@uchicago.edu>
2015-06-10 11:00:55 -0500

    start to scrape out forms

--- service_scraper.py
+++ service_scraper.py
@@ -12,6 +12,7 @@ options = page.xpath("//option")
 
 url = "https://servicerequest.cityofchicago.org/web_intake_chic/Controller?op=query&invSelect="
 
+base_url = "https://servicerequest.cityofchicago.org/web_intake_chic/"
 
 for option in options :
     text = option.text
@@ -28,6 +29,36 @@ for option in options :
     page2 = lxml.html.fromstring(result2.text)
 
     print page2.xpath('.//p[child::b[contains(text(), "Usage Notes")]]')[0].text_content()
+
+    request_url = base_url + page2.xpath('.//a[child::i[contains(text(), "Request this Service")]]')[0].attrib['href']
+
+    print request_url
+
+    result3 = s.get(request_url)
+    
+    #import pdb
+    #pdb.set_trace()
+    page3 = lxml.html.fromstring(result3.text)
+    for row in page3.xpath('.//form')[0].xpath('.//table')[0].xpath('.//tr[.//input|.//select]'):
+        print row.xpath('.//td')[0].text_content()
+        input_one = row.xpath('.//td')[0].xpath('.//input')
+        if input_one:
+            print input_one[0].values()
+        else:
+            print row.xpath('.//td')[0].xpath('.//select')[0].values()
+
+        input_two = row.xpath('.//td')[1].xpath('.//input')
+
+        if input_two:
+            print input_two[0].values()
+        else:
+            print row.xpath('.//td')[1].xpath('.//select')[0].values()
+
+
+    break
+        
+        
+    
 
 
 
